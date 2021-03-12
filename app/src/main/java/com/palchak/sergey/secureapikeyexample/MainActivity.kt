4f147250253835dce70e1d5d42b1297dbee1e61d
Bro Chenzox <spalnavy@gmail.com>
2021-03-12 21:39:47 +0300

    Secure API key from passing it to Git

--- app/src/main/java/com/palchak/sergey/secureapikeyexample/MainActivity.kt
+++ app/src/main/java/com/palchak/sergey/secureapikeyexample/MainActivity.kt
@@ -2,10 +2,14 @@ package com.palchak.sergey.secureapikeyexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        val textView = findViewById<TextView>(R.id.textView)
+//        textView.text = BuildConfig.API_KEY
     }
 }
